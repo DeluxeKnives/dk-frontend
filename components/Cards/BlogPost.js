c7f8b0c930416923a74dacd78a533cbe086b6463
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-10-01 17:49:36 -0500

    initial commit

---
+++ components/Cards/BlogPost.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import { useTranslation } from 'next-i18next';
+import { useText } from '~/theme/common';
+import useStyles from './cards-style';
+
+function BlogPost(props) {
+  const classes = useStyles();
+  const text = useText();
+  const {
+    img,
+    title,
+    desc,
+  } = props;
+  const { t } = useTranslation('common');
+
+  return (
+    <Paper className={classes.post}>
+      <figure>
+        <img src={img} alt="thumb" />
+      </figure>
+      <div className={classes.text}>
+        <Typography variant="h5" className={text.subtitle2}>{title}</Typography>
+        <Typography display="block" component="p" className={text.paragraph}>{desc}</Typography>
+      </div>
+      <Button
+        href="#"
+        color="primary"
+        className={classes.readmore}
+        classes={{
+          root: classes.rootReadmore,
+          text: classes.textReadmore
+        }}
+      >
+        {t('unisex-landing.read_more')}
+      </Button>
+    </Paper>
+  );
+}
+
+BlogPost.propTypes = {
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+};
+
+export default BlogPost;
---
+++ components/SideNavigation/SideNavigation.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Container from '@material-ui/core/Container';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Scrollspy from 'react-scrollspy';
+import { useTranslation } from 'next-i18next';
+import logo from '~/public/images/unisex-logo.svg';
+import useStyles from './sidenav-style';
+import navMenu from './menu';
+
+let counter = 0;
+function createData(name, url, offset) {
+  counter += 1;
+  return {
+    id: counter,
+    name,
+    url,
+    offset,
+  };
+}
+
+function SideNavigation() {
+  const classes = useStyles();
+  const { t } = useTranslation('common');
+  const [menuList] = useState([
+    createData(navMenu[0], '#' + navMenu[0], 160),
+    createData(navMenu[1], '#' + navMenu[1]),
+    createData(navMenu[2], '#' + navMenu[2]),
+    createData(navMenu[3], '#' + navMenu[3], -40),
+    createData(navMenu[4], '#' + navMenu[4]),
+    createData(navMenu[5], '#' + navMenu[5]),
+  ]);
+  return (
+    <div className={classes.navigation}>
+      <Container fixed>
+        <nav className={classes.navMenu}>
+          <AnchorLink href="#home" className={classes.logo}>
+            <img src={logo} alt="logo" />
+          </AnchorLink>
+          <List component="nav" className={classes.menu}>
+            <Scrollspy
+              items={navMenu}
+              currentClassName="active"
+            >
+              {menuList.map(item => (
+                <ListItem
+                  key={item.id.toString()}
+                  button
+                  component={AnchorLink}
+                  offset={item.offset || 0}
+                  href={item.url}
+                  classes={{ root: classes.link }}
+                >
+                  <ListItemText
+                    classes={{ root: classes.text }}
+                    primary={t('unisex-landing.header_' + item.name)}
+                  />
+                </ListItem>
+              ))}
+            </Scrollspy>
+          </List>
+        </nav>
+      </Container>
+    </div>
+  );
+}
+
+export default SideNavigation;
